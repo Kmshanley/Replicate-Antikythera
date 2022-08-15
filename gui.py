f24f531ba77e70be4c802599aed0a433921780b0
Kmshanley <kmshanley01@gmail.com>
2022-08-14 23:12:17 -0400

    Add some simulation Controls
    Layout and add the basic objects for the simulation controls.

--- gui.py
+++ gui.py
@@ -32,6 +32,31 @@ def menu():
     text = Text(infoFrame, width=20, height=20, state='disabled')
     text.grid(column=0, row=0,sticky="n")
 
+    v1 = DoubleVar()
+    silder = ttk.Scale(mainframe, variable = v1, 
+           from_ = 0, to = 1000, 
+           orient = HORIZONTAL, length=solSystem_Background.width())
+    silder.grid(column=1, row=3, sticky="s")
+
+    simulation_currentData_Label = ttk.Label(mainframe, text = "Press play to start simulation")
+    simulation_currentData_Label.grid(column=1, row=2)
+
+    #Create a frame for the pause and play buttons
+    controlsFrame = ttk.Frame(mainframe)
+    controlsFrame.grid(column=1, row=1)
+
+    pauseSim_button = Button(controlsFrame, text="Pause")
+    pauseButton_img = PhotoImage(file="pausebutton.png")
+    pauseButton_img = pauseButton_img.subsample(16,16) #make the button smaller (512/16 = 32)
+    pauseSim_button.config(image=pauseButton_img)
+    pauseSim_button.grid(column=1, row=0)
+
+    startSim_button = Button(controlsFrame, text="Play")
+    playButton_img = PhotoImage(file="playbutton.png")
+    playButton_img = playButton_img.subsample(16,16) #make the button smaller (512/16 = 32)
+    startSim_button.config(image=playButton_img)
+    startSim_button.grid(column=0, row=0)
+
     start_month = StringVar(mainframe, "1")
     start_day = StringVar(mainframe,"1")
     start_year = StringVar(mainframe, "2001")
